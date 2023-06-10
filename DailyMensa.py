6e4be4bba955c669b24f10510091ca0dff282953
MrMinemeet <alex@wtf-my-code.works>
2023-06-10 22:59:00 +0200

    Finished impl. of dailyMensa
    fetches Menu 1, Menu 2 and "Tagesteller" from the mensa page and sends it to the webhook.
    If the current day is on a weekend, it will not do any request to mensen.at

--- DailyMensa.py
+++ DailyMensa.py
@@ -1,14 +1,27 @@
-from bs4 import BeautifulSoup
 import datetime
 import os
+
 import requests
+from bs4 import BeautifulSoup
+
 from Util import send_message
 
 URL = "https://www.mensen.at/"
-COOKIES = {"mensenExtLocation": "1"} # Set cookie for specific mensa entry (1 = JKU Linz Mensa)
+COOKIES = {"mensenExtLocation": "1"}  # Set cookie for specific mensa entry (1 = JKU Linz Mensa)
+
+MESSAGE_TEMPLATE = """__***Mensa JKU Linz - {weekday}***__
+
+**Menu Classic 1**
+{menu_classic_1}
+
+**Menu Classic 2**
+{menu_classic_2}
+
+**Tagesteller**
+{daily_plate}"""
 
 
-def number_to_weekday(number : int) -> str:
+def number_to_weekday(number: int) -> str:
     if number < 1 or number > 7:
         raise ValueError("Number must be between 1 and 7 but was " + str(number))
 
@@ -25,8 +38,6 @@ def number_to_weekday(number : int) -> str:
     return weekdays.get(number)
 
 
-
-
 if __name__ == "__main__":
     # Get WebHook URL from environment variable
     WEBHOOK_URL = os.environ.get("DAILY_MENSA_WEBHOOK_URL")
@@ -34,30 +45,40 @@ if __name__ == "__main__":
     if WEBHOOK_URL is None:
         raise ValueError("Environment variable DAILY_MENSA_WEBHOOK_URL is not set")
 
-
     response = requests.get(URL, cookies=COOKIES, timeout=60)
 
     if response.status_code != 200:
         send_message(WEBHOOK_URL, "Error: " + str(response.status_code))
         exit(1)
 
-
-
     soup = BeautifulSoup(response.text, "html.parser")
 
-    numericDay = datetime.datetime.today().weekday() + 1 # Monday = 1, …, Sunday = 7
+    numericDay = datetime.datetime.today().weekday() + 1  # Monday = 1, …, Sunday = 7
 
-    # Get menu-category
-    menu_categories = soup.findAll("div", {"class": "menu-category"})
-    # Remove the first (mobile version)
-    menu_categories.pop(0)
+    message = ""
+    if numericDay >= 6:
+        numericDay = 1
+        # TODO: Set it to the next Monday if it is Saturday or Sunday
+        send_message(WEBHOOK_URL, "**The mensa is closed today.**")
+        exit(0)
 
     # Left (Menu 1 + Daily Plate)
-    menu_category = menu_categories[0]
-    # Get menu_item
-    menu_items = menu_category.findAll("div", {"class": "menu-item-" + str(numericDay)})
+    menu_categories = soup.find("div", {"class": "menu-left"}).find("div", recursive=False)
+    menu_items = menu_categories.find_all("div", {"class": "menu-item-" + str(numericDay)})
+    menu_classic_1 = menu_items[0].text.strip()
+    daily_plate = menu_items[1].text.strip()
 
+    # Right (Menu 2) get item
+    menu_categories = soup.find("div", {"class": "menu-right"}).find("div", recursive=False)
+    menu_items = menu_categories.find("div", {"class": "menu-item-" + str(numericDay)})
+    menu_classic_2 = menu_items.text.strip()
 
+    # Send message
+    message += MESSAGE_TEMPLATE.format(
+        weekday=number_to_weekday(numericDay),
+        menu_classic_1="> " + menu_classic_1.split("\n", 1)[1].replace("\n", "\n> "),
+        menu_classic_2="> " + menu_classic_2.split("\n", 1)[1].replace("\n", "\n> "),
+        daily_plate="> " + daily_plate.split("\n", 1)[1].replace("\n", "\n> ")
+    )
 
-
-    send_message(WEBHOOK_URL, "Mensa JKU Linz - " + number_to_weekday(numericDay))
+    send_message(WEBHOOK_URL, message)
